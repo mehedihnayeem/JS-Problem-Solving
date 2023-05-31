b245cebb4bf07747a9237eb4c24fe7ed492b7db6
Mehedi Nayeem <87238832+mehedihnayeem@users.noreply.github.com>
2023-05-31 17:52:58 +0600

    Update 02.file-one-simplied-version.js

--- Problem Solving 2 - LWS/02.file-one-simplied-version.js
+++ Problem Solving 2 - LWS/02.file-one-simplied-version.js
@@ -1,3 +1,5 @@
+// Solved by Sumit Saha
+
 const sentence =
   "The challenge of AI in Bangladesh is both promising and complex. As a country with a burgeoning tech industry, Bangladesh recognizes the transformative potential of artificial intelligence in various sectors. However, there are several hurdles to overcome. Firstly, there is a need to develop a skilled workforce proficient in AI technologies to drive innovation and implementation. Additionally, there is a requirement for robust infrastructure and resources to support AI research and development. Ethical considerations, data privacy, and security are also critical concerns. Despite these challenges, Bangladesh is actively embracing AI, aiming to leverage its potential for economic growth, improved services, and sustainable development in various domains.";
 
